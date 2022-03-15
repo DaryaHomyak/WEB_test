188a57e211fb60c3860c514481ece9e703c0899a
User <aaredov@bk.ru>
2022-03-15 19:52:32 +0300

    commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hi  ^_^<h1>'
+    return '<h1>                                                                                          Hi  ^_^<h1>'
 
 
 if __name__ == '__main__':
